b1962557abc7b2edfd7ca6d165ee114b04358a8b
DanilkaShalinda <danilkashalindadsd@gmail.com>
2023-06-05 19:56:00 +0530

    create colour change buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useState} from "react";
+
+const Person =(props)=>{
+    return (
+      <>
+          <h1>First Name : {props.fname}</h1>
+          <h2>Last Name : {props.lname}</h2>
+          <h2>Age : {props.age}</h2>
+      </>
+    );
+}
+
+
+const App = () => {
+    const [counter , setcounter] =useState(0);
+    const [Colour ,setColour] = useState('');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <di className='App'>
+
+          <h1>
+            <Person
+                fname={'john'}
+                lname={'cina'}
+                age={36}
+            />
+
+
+          <di>
+              <h1>Counter</h1>
+              <button onClick={()=>{setcounter((prevCount)=>prevCount + 5)}}>+</button>
+              <h1>{counter}</h1>
+              <button onClick={() => { setcounter((prevCount) => prevCount -5)}}>-</button>
+          </di>
+          <di>
+              <h1>My Favorite colour Is {Colour}</h1>
+              <button onClick={()=>setColour('Green')}>Green</button>
+              <button onClick={()=>setColour('Blue')}>Blue</button>
+              <button onClick={()=>setColour('Red')}>Red</button>
+              <button onClick={()=>setColour('Pink')}>Pink</button>
+
+          </di>
+          </h1>
+      </di>
+
   );
 }
 
-export default App;
+export default App ;
